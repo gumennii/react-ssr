6141c36eb5b8aa245b284461e00574c5c8af8500
gumennii <mihail.gumennii@gmail.com>
2018-08-24 23:50:55 -0700

    add static routes

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
+import { StaticRouter } from 'react-router-dom'
+import Routes from '../client/Routes'
 
-import Home from '../client/components/Home'
+export default (req) => {
+  const context = {}
 
-export default () => {
-  const content = renderToString(<Home />)
+  const content = renderToString(
+    <StaticRouter location={req.path} context={context}>
+      <Routes />
+    </StaticRouter>
+  )
 
   return `
     <!DOCTYPE html>
